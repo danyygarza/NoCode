4cae6f73a1266dae1d9bbba356a833143f3db3b8
Victor Ponce <victor@192.168.1.7>
2022-10-07 11:43:14 -0500

    working on submit data from fform

--- src/Pages/SandBox/code.js
+++ src/Pages/SandBox/code.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 import Test from '../../components/Forms/Test/test'
 import Excel from '../../components/Forms/Excel/Excel';
 import templateExcel from '../../components/Forms/TemplateForm/templateExcel';
-
+import Button from 'antd'
 function Code() {
 
     const [forms, setForms] = useState([]);
 
     const finish = (values) => {
+        console.log("inside the finish function")
         console.log(values);
     }
 
@@ -16,7 +17,7 @@ function Code() {
             return ({ form });
         }),
         //!this will be the component of dany
-        <Excel />
+        <Excel finish={finish} />
     )
 
 }
