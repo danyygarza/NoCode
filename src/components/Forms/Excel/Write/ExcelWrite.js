d2e72c374e4df1a6d21298b601802b07e0f40440
danygzatec <62119447+danygzatec@users.noreply.github.com>
2022-10-18 17:33:33 -0500

    fixes to excelwrite

--- src/components/Forms/Excel/Write/ExcelWrite.js
+++ src/components/Forms/Excel/Write/ExcelWrite.js
@@ -1,13 +1,47 @@
 import React, { useState, useEffect, useImperativeHandle, forwardRef } from "react"
-import { Form, Input, Checkbox, Button, Menu, Dropdown, Space, Col, Row, Typography } from 'antd'
+import { Form, Input, Checkbox, Button, Menu, Dropdown, Space, Col, Row, Typography, Popover } from 'antd'
 import { MinusCircleOutlined, PlusOutlined, SmileOutlined, DownOutlined } from '@ant-design/icons';
-import { Done } from "@mui/icons-material";
+import { Done, Splitscreen } from "@mui/icons-material";
 import Test from "../../Test/test";
 import "../RemoveDuplicate/RemoveDuplicate.css";
+import data from '../../syntax.json'
 
 let id = Date.now();
 const { Title } = Typography;
+const buttonWidth = 70;
 
+const description = (
+    <>
+        {data.fridaExcelReadersSyntaxWrite.Description}
+    </>
+);
+
+const parameters = (
+    <>
+        {data.fridaExcelReadersSyntaxWrite.Parameters}
+    </>
+);
+
+const syntax = (
+    <>
+        {data.fridaExcelReadersSyntaxWrite.Syntax1}<br />
+        {data.fridaExcelReadersSyntaxWrite.Syntax2}<br />
+        {data.fridaExcelReadersSyntaxWrite.Syntax3}<br />
+        {data.fridaExcelReadersSyntaxWrite.Syntax4}<br />
+    </>
+);
+
+const examples = (
+    <>
+        {data.fridaExcelReadersSyntaxWrite.Example1}<br />
+        {data.fridaExcelReadersSyntaxWrite.Example2}<br />
+        {data.fridaExcelReadersSyntaxWrite.Example3}<br />
+        {data.fridaExcelReadersSyntaxWrite.Example4}<br />
+        {data.fridaExcelReadersSyntaxWrite.Example5}<br />
+        {data.fridaExcelReadersSyntaxWrite.Example6}<br />
+        {data.fridaExcelReadersSyntaxWrite.Example7}<br />
+    </>
+)
 
 function ExcelWrite(props) {
     console.log("submit in write is", props.submit)
@@ -46,6 +80,11 @@ function ExcelWrite(props) {
             </Row></>
     ]);
 
+    const remove = () => {
+        const values = [...inputs];
+        values.splice = (id, 1);
+        setInputs(values);
+    }
 
     const add = () => {
         setInputs([...inputs, <Row>
@@ -76,6 +115,15 @@ function ExcelWrite(props) {
                 >
                     <Input type="text" placeholder={"<ColRow>"} />
                 </Form.Item >
+            </Col>
+            <Col offset={12}>
+                <Button
+                    type="solid"
+                    onClick={() => remove()}
+                    shape="circle"
+                    icon={<MinusCircleOutlined />}
+                >
+                </Button>
             </Col>
         </Row>])
         setClick(true);
@@ -109,6 +157,26 @@ function ExcelWrite(props) {
                 <Row justify="center">
                     <Col>
                         <Title level={5}>ExcelWrite</Title>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="demo">
+                            <div style={{ marginLeft: buttonWidth, whiteSpace: 'nowrap' }}>
+                                <Popover placement="topLeft" title="Description" content={description} trigger="click" className='popover-position'>
+                                    <Button>Description</Button>
+                                </Popover>
+                                <Popover placement="topLeft" title="Parameters" content={parameters} trigger="click" className='popover-position'>
+                                    <Button>Parameters</Button>
+                                </Popover>
+                                <Popover placement="top" title="Syntax" content={syntax} trigger="click" className='popover-position'>
+                                    <Button>Syntax</Button>
+                                </Popover>
+                                <Popover placement="topRight" title="Examples" content={examples} trigger="click" >
+                                    <Button>Examples</Button>
+                                </Popover>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
                 {inputs.map((input) => {
