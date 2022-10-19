5b3dbdf54b887fbac2fd640aa0d9fa83e280eab9
Victor David Ponce Quintanilla <victor.ponce@softtek.com>
2022-10-19 13:10:49 -0500

    fix

--- src/Pages/SandBox/code.js
+++ src/Pages/SandBox/code.js
@@ -1,16 +1,10 @@
 import React, { useRef, useState } from 'react'
 import { Button, Row, Col } from 'antd'
-import { useForm } from 'antd/lib/form/Form';
-import {SaveOutlined} from '@ant-design/icons'
-
-import Forms from "../../components/Forms/Forms";
 import Frida from '../../components/FRIDA/FRIDA';
-import Tutorial from '../../components/ProcesoNuevo/Tutorial';
 
 //TODO: create an alert and show in which from is not completed 
 function Code() {
     const [forms, setForms] = useState([]);
-    const [code, setCode] = useState([]);
     //! this to call functions from Frida (child component) // 
 
     const ref = useRef(null)
@@ -23,9 +17,6 @@ function Code() {
     return (
         <>
             <Row>
-                <SaveOutlined />
-            </Row>
-            <Row>
                 <Col offset={8}>
                     {/* //!this is the place where all the form will be stored */}
                     <Frida ref={ref} />
@@ -33,15 +24,11 @@ function Code() {
             </Row>
             <Row>
                 <Col offset={21}>
-                    <Button className='forms-button2' onClick={(event) => {
-                        console.log("button from code is being pressed")
-                        submitRef.current.showAlert();
-                    }}>
+                    <Button type="primary" onClick={handleValidate} >
                         Submit from code!!
                     </Button>
                 </Col>
             </Row>
-
         </>
     )
 }
