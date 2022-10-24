cc80f61744601dce3c6298d7137846208adc7984
danygzatec <62119447+danygzatec@users.noreply.github.com>
2022-10-24 16:51:34 -0500

    updates

--- src/Test/testForm.js
+++ src/Test/testForm.js
@@ -1,11 +1,13 @@
 import React, { useState, useForm } from "react";
-import { Form, Input, Card, Modal } from "antd";
+import { Form, Input, Card, Modal, Button, Popover, Row, Col } from "antd";
 
 const { Meta } = Card;
 
 export default function Testform(props) {
     const [open, setOpen] = useState(false);
     const [form] = Form.useForm();
+
+    const buttonWidth = 70;
 
     const onFinish = (values) => {
         console.log("Success:", values);
@@ -21,7 +23,7 @@ export default function Testform(props) {
     console.log(test.forms)
     return (
         <>
-            < Card hoverable style={{ width: 240, }
+            < Card hoverable style={{ width: 800, }
             } cover={< img alt="excel icon" src="../../../../excelIcon.ico" />} onClick={() => setOpen(true)} maskClosable={true} >
                 <Meta title="Excel Write" description="Write something in a given cell in a worksheet." />
             </Card >
@@ -34,38 +36,65 @@ export default function Testform(props) {
                 width={900}
             >
                 <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed} id={id}>
-
-
+                    <Row>
+                        <Col>
+                            <div className="demo">
+                                <div style={{ marginLeft: buttonWidth, whiteSpace: 'nowrap' }}>
+                                    <Popover placement="topLeft" title="Description" /*content={description}*/ trigger="click" className='popover-position'>
+                                        <Button>Description</Button>
+                                    </Popover>
+                                    <Popover placement="topLeft" title="Parameters" /*content={parameters}*/ trigger="click" className='popover-position'>
+                                        <Button>Parameters</Button>
+                                    </Popover>
+                                    <Popover placement="top" title="Syntax" /*content={syntax}*/ trigger="click" className='popover-position'>
+                                        <Button>Syntax</Button>
+                                    </Popover>
+                                    <Popover placement="topRight" title="Examples" /*content={examples}*/ trigger="click">
+                                        <Button>Examples</Button>
+                                    </Popover>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
                     {test.forms.map((item) => {
                         return (
                             <>
-                                {console.log(item[1])}
-                                {/* //!  for text inputs */}
-                                {
-                                    item[1] === "text" &&
-                                    <Form.Item
-                                        name={[`write` + id, item[0]]}
-                                        label= {item[0]}
-                                        rules={[{ required: true, message: 'Please input your username!' }]}
-                                    >
-                                        <Input type="text" placeholder={item[0]} onChange={props.onChange} />
-                                    </Form.Item>}
-                                {/* //!  for text file picker */}
-                                {/* {item[1] === "file" &&
-                                <Form.Item
-                                    name={[`${item[0] + id}`, `${item[0]}`]}
-                                    label={item[0]}
-                                    rules={[{ required: true, message: 'Please input your username!' }]}
-                                >
-                                </Form.Item>} */}
-                                {/* //!  for text date */}
-                                {/* {item[1] === "date" &&
-                                <Form.Item
-                                    name={[`${item[0] + id}`, `${item[0]}`]}
-                                    label={item[0]}
-                                    rules={[{ required: true, message: 'Please input your username!' }]}
-                                >
-                                </Form.Item>} */}
+                                <div>
+
+                                    {console.log(item[1])}
+                                    {/* //!  for text inputs */}
+                                    <Col span={8}>
+                                        {
+                                            item[1] === "text" &&
+                                            <Form.Item
+                                                name={[`write` + id, item[0]]}
+                                                label={item[0]}
+                                                rules={[{ required: true, message: 'Please input your username!' }]}
+                                                style={{ width: 'auto' }}
+                                            >
+                                                <Input type="text" placeholder={item[0]} onChange={props.onChange} />
+                                            </Form.Item>
+
+                                        }
+                                    </Col>
+                                    {/* //!  for text file picker */}
+                                    {/* {item[1] === "file" &&
+                                        <Form.Item
+                                            name={[`${item[0] + id}`, `${item[0]}`]}
+                                            label={item[0]}
+                                            rules={[{ required: true, message: 'Please input your username!' }]}
+                                        >
+                                        </Form.Item>} */}
+                                    {/* //!  for text date */}
+                                    {/* {item[1] === "date" &&
+                                        <Form.Item
+                                            name={[`${item[0] + id}`, `${item[0]}`]}
+                                            label={item[0]}
+                                            rules={[{ required: true, message: 'Please input your username!' }]}
+                                        >
+                                        </Form.Item>} */}
+
+                                </div>
                             </>
                         )
                     })}
