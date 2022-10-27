72e30c79d5192e9a3cb0859e4c11c51009a6eaea
FelonRed <felonred@pop-os.localdomain>
2022-10-27 15:15:33 -0500

    adds functining search

--- src/Test/ListTest.js
+++ src/Test/ListTest.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import items from "../components/Forms/AllFunctions";
 import data from "../components/Forms/syntax.json";
-
+function searchFunction(input,nameSearch){
+    
+    
+    try{
+        
+        let text = input.toUpperCase()
+        const name = nameSearch.toUpperCase()
+        console.log('try', name.search(text) > -1)
+        return(name.search(text) > -1)
+    }catch(e){
+        console.log('try', e.message)
+        return(false)
+    }
+    
+}
 function ListTest(props) {
-    console.log('input', props.input)
-    console.log('search', data[0].Name)
+    console.log('input',typeof props.input)
+    console.log('search', data[0].Name.search('x'))
   const filteredData = data.filter((el) => 
     //if no input the return the original
 
-    el.Name === 'Excel Write'
+    searchFunction(props.input,el.Name)
     
     // try {
     //   if (props.input === "") {
