c07f053dee8d7eeda04688774a5baa292bf27739
Victor Ponce <victor@192.168.1.4>
2022-10-26 20:09:44 -0500

    code generation works

--- src/Test/testForm.js
+++ src/Test/testForm.js
@@ -5,6 +5,7 @@ import React, { useState, useForm, useEffect } from "react";
 import { Form, Input, Card, Modal, Button, Popover, Row, Col, Tabs, Radio } from "antd";
 import { PlusOutlined } from '@ant-design/icons';
 import { useModalForm } from 'sunflower-antd';
+import { optionGroupUnstyledClasses } from '@mui/base';
 const { Meta } = Card;
 
 export default function Testform(props) {
@@ -24,7 +25,6 @@ export default function Testform(props) {
         console.log("Failed:", errorInfo);
         props.setSubmit(false)
     };
-    let id = Date.now();
 
     const {
         modalProps,
@@ -35,34 +35,42 @@ export default function Testform(props) {
         formResult,
     } = useModalForm({
         defaultVisible: false,
-        autoSubmitClose: true,
-        autoResetForm: true,
-        async submit(data) {
-            console.log('beforeSubmit');
-            await new Promise(r => setTimeout(r, 1000));
-            console.log('afterSubmit', data);
+        autoSubmitClose: false,
+        autoResetForm: false,
+        submit(data) {
+            let codeArr = [];
+            let inputs = Object.values(data) // {key, input}
+            let tempString = "";
+            let temp = [];
+            let i = 0;
+            inputs.forEach((obj) =>{
+                if(i === 2){
+                    i = 0; 
+                    console.log(tempString);
+                    codeArr.push(tempString);
+                    inputs.splice(0, forms.length);
+                }
+                else{
+                    console.log(`${(Object.keys(obj))} ${(Object.values(obj))} `);
+                    tempString.length === 0 ? tempString = `${(Object.keys(obj))} ${(Object.values(obj))} ` : tempString += `${(Object.keys(obj))} ${(Object.values(obj))} `;
+                    i++;
+                }
+            })
+
+            console.log("code arr", codeArr);
             return 'ok';
         },
         form,
     });
-    
+
     const add = () => {
-        // setForm(...forms,Object.values(props.data.forms)[key])
-        // Object.values(props.data.forms)[key].map((item)=>{
-        //     const tempArray = []
-        //     forms.push(item)
-        //     setForm(item);  
-
-        // })
-        // console.log(forms); 
-
-        setForms(forms.concat(item)); 
+        setForms(forms.concat(item));
 
     }
 
     useEffect(() => {
         const tempArray = []
-        Object.values(props.data.forms)[key].map((item)=>{
+        Object.values(props.data.forms)[key].map((item) => {
             tempArray.push(item)
         })
         setItem(tempArray)
@@ -70,46 +78,46 @@ export default function Testform(props) {
         console.log(forms)
     }, [forms])
 
-    
-    console.log(Object.keys(props.data.forms))
+
     return (
         <>
             < Card hoverable style={{ width: 800, }
             } cover={< img alt="excel icon" src="../../../../excelIcon.ico" />} onClick={() => setOpen(true)} maskClosable={true} >
                 <Meta title="Excel Write" description="Write something in a given cell in a worksheet." />
             </Card >
-            <Modal {...modalProps} title="useModalForm" open={open} onOk={() => setOpen(false)} onCancel={() => setOpen(false)} okText="submit" width={1200} >
+            <Modal {...modalProps} title="useModalForm" open={open} onCancel={() => setOpen(false)} okText="submit" width={1200} height={800}>
                 <>
                     <p>
                         submit: username {formValues.username} email {formValues.email}
                     </p>
                     <p>result: {formResult}</p>
                     <Form layout="inline" {...formProps}>
-                        <Radio.Group defaultValue="a" buttonStyle="solid" onChange={(e) => setKey(e.target.value)}>
-                            {Object.keys(props.data.forms).map((_, index) => {
-                                return (
-                                    <Radio.Button value={index}>syntax: {index}</Radio.Button>
-                                )
-                            })}
-                        </Radio.Group>
                         <Row>
-                            {forms.map((item) => {
-                                console.log("map", item); 
+                            <Col>
+                                <Radio.Group defaultValue="a" buttonStyle="solid" onChange={(e) => setKey(e.target.value)}>
+                                    {Object.keys(props.data.forms).map((_, index) => {
+                                        return (
+                                            <Radio.Button value={index}>syntax: {index}</Radio.Button>
+                                        )
+                                    })}
+                                </Radio.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            {forms.map((item, index) => {
                                 return (
                                     <>
-                                        {console.log(item.type)}
                                         {
                                             item.type === "text" &&
                                             <>
                                                 <Col>
-                                                    <Form.Item name={[`${item.title}` + id, item.title]} label={item.title} rules={[{ required: true, message: 'Please input your username!' }]} style={{ width: 'auto' }}>
-                                                        <Input type={item.type} placeholder={item.title} onChange={props.onChange} />
+                                                    <Form.Item name={[`${item.title}  ${index}`, item.title]} label={item.title} rules={[{ required: true, message: 'Please fill this out' }]} style={{ width: 'auto' }}>
+                                                        <Input type={item.type} placeholder={item.placeHolder} onChange={props.onChange} />
                                                     </Form.Item>
                                                 </Col>
                                             </>
                                         }
                                     </>
-                                    // <h1>test</h1>
                                 )
                             })}
 
