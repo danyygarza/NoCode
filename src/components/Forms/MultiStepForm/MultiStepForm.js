ed3db045c9a730493ce023c16f49687b698bd2b1
redeyeruiz <A01366792@tec.mx>
2022-11-21 15:12:50 -0600

    multi step form

--- src/components/Forms/MultiStepForm/MultiStepForm.js
+++ src/components/Forms/MultiStepForm/MultiStepForm.js
@@ -1,62 +1,134 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Button, message, Steps } from "antd";
 import "antd/dist/antd.css";
 import Reader from "./reader";
 import GroupSelect from "./GroupSelect";
+import Testform from "../../../Test/testForm";
+import { getFirestore, doc, getDoc } from "@firebase/firestore";
+const db = getFirestore();
 
-function RenderFunctions(functions, group, collection) {
-  console.log('render', functions)
-  return functions.functions
-    .filter(
-      (element) =>
-        element.collection === functions.collection &&
-        element.group == functions.group
-    )
-    .map((item) => (
-      <>
-        {console.log('item', item)}
-        <Button
-          style={{
-            height: 120,
-            borderRadius: 40,
-            borderColor: "white",
-          }}
-          onClick={(element) => {
-            // add(item);
-          }}
-        >
-          <div className="imgp">
-            <img src="favicon.ico" alt="logo" style={{ width: 70 }} />
-            <p style={{ color: "black", marginLeft: 0 }}>
-              <b>{item.function}</b>
-            </p>
-          </div>
-        </Button>
-      </>
-    ));
-}
+
+// function RenderFunctions(functions, group, collection) {
+//   console.log("render", functions);
+//   return functions.functions
+//     .filter(
+//       (element) =>
+//         element.collection === functions.collection &&
+//         element.group == functions.group
+//     )
+//     .map((item) => (
+//       <>
+//         {console.log("item", item)}
+//         <Button
+//           style={{
+//             height: 120,
+//             borderRadius: 40,
+//             borderColor: "white",
+//           }}
+//           onClick={(element) => {
+//             //add(item);
+//             console.log('click')
+//           }}
+//         >
+//           <div className="imgp">
+//             <img src="favicon.ico" alt="logo" style={{ width: 70 }} />
+//             <p style={{ color: "black", marginLeft: 0 }}>
+//               <b>{item.function}</b>
+//             </p>
+//           </div>
+//         </Button>
+//       </>
+//     ));
+// }
 
 function MultiStepForm(props) {
+  const add = async (data) => {
+    const colRef = doc(db, data.collection, data.function);
+    const docSnap = await getDoc(colRef);
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      props.setForms([
+        ...props.forms,
+        {
+          id: props.id,
+          form: (
+            <Testform
+              function={data.function}
+              data={docSnap.data()}
+              variables={props.variables}
+              setVariables={props.setVariables}
+              code={props.code}
+              setCode={props.setCode}
+              id={props.id}
+            />
+          ),
+        },
+      ]);
+      props.setId(props.id + 1);
+      // props.setNumberList([...props.numberList, props.id]);
+    } else {
+      console.log("no such document!");
+    }
+  };
+
+  function RenderFunctions(functions, group, collection) {
+    console.log("render", functions);
+    return functions.functions
+      .filter(
+        (element) =>
+          element.collection === functions.collection &&
+          element.group == functions.group
+      )
+      .map((item) => (
+        <>
+          {console.log("item", item)}
+          <Button
+            style={{
+              height: 120,
+              borderRadius: 40,
+              borderColor: "white",
+            }}
+            onClick={() => {
+              add(item);
+              console.log('click', item)
+            }}
+          >
+            <div className="imgp">
+              <img src="favicon.ico" alt="logo" style={{ width: 70 }} />
+              <p style={{ color: "black", marginLeft: 0 }}>
+                <b>{item.function}</b>
+              </p>
+            </div>
+          </Button>
+        </>
+      ));
+  }
+
   const [current, setCurrent] = useState(0);
   const [collection, setCollection] = useState("null");
   const [group, SetGroup] = useState("null");
   const steps = [
     {
       title: "Choose the reader",
-      content: <Reader collection={collection} setCollection={setCollection}/>,
+      content: <Reader collection={collection} setCollection={setCollection} />,
     },
     {
       title: "Choose the function group",
-      content: <GroupSelect group={group} SetGroup={SetGroup}/>,
+      content: <GroupSelect group={group} SetGroup={SetGroup} add={add}/>,
     },
     {
       title: "Choose the function",
-      content: <RenderFunctions functions={props.functions} collection={collection} group={group}/>,
+      content: (
+        <RenderFunctions
+          functions={props.functions}
+          collection={collection}
+          group={group}
+        />
+      ),
     },
   ];
-  
-  const next = () => {
 
+  const next = () => {
     setCurrent(current + 1);
   };
   const prev = () => {
@@ -68,16 +140,20 @@ function MultiStepForm(props) {
   }));
 
   useEffect(() => {
-    console.log('group', group)
-  }, [])
+    console.log("group", group);
+  }, []);
 
   return (
     <>
-      <Steps current={current} items={items} style={{padding: 25}}/>
+      <Steps current={current} items={items} style={{ padding: 25 }} />
       <div className="steps-content">{steps[current].content}</div>
       <div className="steps-action">
         {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()} style={{justifyContent: 'right'}}>
+          <Button
+            type="primary"
+            onClick={() => next()}
+            style={{ justifyContent: "right" }}
+          >
             Next
           </Button>
         )}
