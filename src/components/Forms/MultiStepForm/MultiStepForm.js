686aac79eb8fe3cb6f1e9d31517714ea6d1b6bd4
redeyeruiz <A01366792@tec.mx>
2022-11-22 15:44:48 -0600

    removed ass

--- src/components/Forms/MultiStepForm/MultiStepForm.js
+++ src/components/Forms/MultiStepForm/MultiStepForm.js
@@ -1,32 +1,148 @@
-import React, { useState, useEffect} from "react";
-import { Button, message, Steps } from "antd";
+import React, { useState, useEffect } from "react";
+import {
+  Steps,
+  Form,
+  Input,
+  Card,
+  Modal,
+  Button,
+  Popover,
+  Row,
+  Col,
+  Tabs,
+  Radio,
+  message,
+  Upload,
+} from "antd";
 import "antd/dist/antd.css";
 import Reader from "./reader";
 import GroupSelect from "./GroupSelect";
-import RenderFunctions from "./functions";
+import Testform from "../../../Test/testForm";
+import { getFirestore, doc, getDoc } from "@firebase/firestore";
+const db = getFirestore();
 
 function MultiStepForm(props) {
-  console.log('funcs', props.functions)
+  console.log("funcs", props.functions);
   const [current, setCurrent] = useState(0);
   const [collection, setCollection] = useState("");
   const [group, SetGroup] = useState("");
+
+  // const add = () => {
+  //   const temp = [];
+  //   Object.values(props.data.forms)[key].map((item, index) => {
+  //     console.log(item);
+  //     switch (item.type) {
+  //       case "text":
+  //         temp.push(
+  //           <>
+  //             {
+  //               <>
+  //                 <Col>
+  //                   <Form.Item
+  //                     name={[`${forms.length}${index}`, item.PlaceHolder]}
+  //                     label={item.title}
+  //                     rules={[
+  //                       { required: true, message: "Please fill this out" },
+  //                     ]}
+  //                     style={{ width: "auto" }}
+  //                   >
+  //                     <Input
+  //                       type={item.type}
+  //                       placeholder={item.placeHolder}
+  //                       onChange={props.onChange}
+  //                       name={index}
+  //                     />
+  //                   </Form.Item>
+  //                 </Col>
+  //               </>
+  //             }
+  //           </>
+  //         );
+  //         break;
+  //       case "filepicker":
+  //         temp.push(
+  //           <>
+  //             {
+  //               <>
+  //                 <Col>
+  //                   <Upload {...info}>
+  //                     <Button icon={<UploadOutlined />}>Click to Upload</Button>
+  //                   </Upload>
+  //                 </Col>
+  //               </>
+  //             }
+  //           </>
+  //         );
+  //         break;
+  //       case "word":
+  //         temp.push(
+  //           <>
+  //             {
+  //               <>
+  //                 <p style={{ color: "black" }}>{item.title}</p>
+  //               </>
+  //             }
+  //           </>
+  //         );
+  //         break;
+  //     }
+  //   });
+  //   setForms([...forms, temp]);
+  // };
+  function RenderFunctions(functions, group, collection) {
+    console.log("render", functions);
+    return functions.functions
+      .filter(
+        (element) =>
+          element.collection === functions.collection &&
+          element.group == functions.group
+      )
+      .map((item) => (
+        <>
+          {console.log("item", item)}
+          <Button
+            style={{
+              height: 120,
+              borderRadius: 40,
+              borderColor: "white",
+            }}
+            onClick={() => {
+              //add(item);
+              console.log("click", item);
+            }}
+          >
+            <div className="imgp">
+              <img src="favicon.ico" alt="logo" style={{ width: 70 }} />
+              <p style={{ color: "black", marginLeft: 0 }}>
+                <b>{item.function}</b>
+              </p>
+            </div>
+          </Button>
+        </>
+      ));
+  }
   const steps = [
     {
       title: "Choose the reader",
-      content: <Reader collection={collection} setCollection={setCollection}/>,
+      content: <Reader collection={collection} setCollection={setCollection} />,
     },
     {
       title: "Choose the function group",
-      content: <GroupSelect group={group} SetGroup={SetGroup}/>,
+      content: <GroupSelect group={group} SetGroup={SetGroup} />,
     },
     {
       title: "Choose the function",
-      content: <RenderFunctions functions={props.functions} collection={collection} group={group}/>,
+      content: (
+        <RenderFunctions
+          functions={props.functions}
+          collection={collection}
+          group={group}
+        />
+      ),
     },
   ];
-  
-  const next = () => {
 
+  const next = () => {
     setCurrent(current + 1);
   };
   const prev = () => {
@@ -38,16 +154,22 @@ function MultiStepForm(props) {
   }));
 
   useEffect(() => {
-    console.log('group', group)
-  }, [])
+    console.log("group", group);
+  }, []);
 
   return (
     <>
-      <Steps current={current} items={items} style={{padding: 25}}/>
-      <div style={{marginTop: 10, marginLeft: 140, marginBottom: 15}}>{steps[current].content}</div>
+      <Steps current={current} items={items} style={{ padding: 25 }} />
+      <div style={{ marginTop: 10, marginLeft: 140, marginBottom: 15 }}>
+        {steps[current].content}
+      </div>
       <div className="steps-action">
         {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()} style={{justifyContent: 'right'}}>
+          <Button
+            type="primary"
+            onClick={() => next()}
+            style={{ justifyContent: "right" }}
+          >
             Next
           </Button>
         )}
