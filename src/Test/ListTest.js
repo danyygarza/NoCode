59330424a1ae1d3ebf85acf345025fcce3768f11
redeyeruiz <A01366792@tec.mx>
2022-11-22 13:19:19 -0600

    merge

--- src/Test/ListTest.js
+++ src/Test/ListTest.js
@@ -21,76 +21,73 @@ function searchFunction(input, nameSearch) {
   }
 }
 function ListTest(props) {
-  
   const filteredData = props.functions.filter((el) =>
     searchFunction(props.input, el.id)
   );
 
-
   const add = async (data) => {
     const colRef = doc(db, data.collection, data.id);
-  const filteredData = props.functions.filter((el) =>
-    searchFunction(props.input, el.function)
-  );
-  //
-  const db = getFirestore();
+    const filteredData = props.functions.filter((el) =>
+      searchFunction(props.input, el.function)
+    );
+    //
+    const db = getFirestore();
 
-  const add = async (data) => {
-    const colRef = doc(db, data.collection, data.function);
-    const docSnap = await getDoc(colRef);
-    if (docSnap.exists()) {
-      console.log(docSnap.data());
-      props.setForms([
-        ...props.forms,
-        {
-          id: props.id,
-          form: (
+    const add = async (data) => {
+      const colRef = doc(db, data.collection, data.function);
+      const docSnap = await getDoc(colRef);
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        props.setForms([
+          ...props.forms,
+          {
+            id: props.id,
+            form: (
+              <Testform
+                function={data.function}
+                data={docSnap.data()}
+                variables={props.variables}
+                setVariables={props.setVariables}
+                code={props.code}
+                setCode={props.setCode}
+                id={props.id}
+                status={props.status}
+                setStatus={props.setStatus}
+                setUpdate={props.setUpdate}
+              />
+            ),
+          },
+        ]);
+        props.setId(props.id + 1);
+        // props.setNumberList([...props.numberList, props.id]);
+      } else {
+        console.log("no such document!");
+      }
+    };
+    console.log("filter", filteredData);
 
-            <Testform
-              function={data.function}
-              data={docSnap.data()}
-              variables={props.variables}
-              setVariables={props.setVariables}
-              code={props.code}
-              setCode={props.setCode}
-              id={props.id}
-              status={props.status}
-              setStatus={props.setStatus}
-              setUpdate={props.setUpdate}
-            />
-          ),
-        },
-      ]);
-      props.setId(props.id + 1);
-      // props.setNumberList([...props.numberList, props.id]);
-    } else {
-      console.log("no such document!");
-    }
+    return (
+      <div style={{ display: "flex" }}>
+        {filteredData.map((item) => (
+          <>
+            {" "}
+            <Button
+              style={{ height: 120, borderRadius: 40, borderColor: "white" }}
+              onClick={(event) => {
+                add(item);
+              }}
+            >
+              <div className="imgp">
+                <img src="favicon.ico" alt="logo" style={{ width: 70 }} />
+                <p style={{ color: "black", marginLeft: 0 }}>
+                  <b>{item.function}</b>
+                </p>
+              </div>
+            </Button>
+          </>
+        ))}
+      </div>
+    );
   };
-  console.log("filter", filteredData);
-
-  return (
-    <div style={{ display: "flex" }}>
-      {filteredData.map((item) => (
-        <>
-          {" "}
-          <Button
-            style={{ height: 120, borderRadius: 40, borderColor: "white" }}
-            onClick={(event) => {
-              add(item);
-            }}
-          >
-            <div className="imgp">
-              <img src="favicon.ico" alt="logo" style={{ width: 70 }} />
-              <p style={{ color: "black", marginLeft: 0 }}>
-                <b>{item.function}</b>
-              </p>
-            </div>
-          </Button>
-        </>
-      ))}
-    </div>
-  );
 }
-
 export default ListTest;
